5735444c3bc35a6c0826d9b010bb700816218ff1
Vladimir Gruzdev <diablo_96_96@mail.ru>
2024-03-26 21:17:22 +0500

    Герои вместе с фракциями

--- app/Http/Resources/FactionsResource.php
+++ app/Http/Resources/FactionsResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Helpers\FactionHelper;
+use App\Models\RefFactions;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,14 +16,14 @@ class FactionsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-
-//        dd($request);
-
+        $faction = RefFactions::where('id', $this->id)->first();
+        $factionHelper = new FactionHelper($faction);
         return [
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
-            'image' => $this->image
+            'image' => $this->image,
+            'heroes' => $factionHelper->getFactionHeroes()
         ];
     }
 }
